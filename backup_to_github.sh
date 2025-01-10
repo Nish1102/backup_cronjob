fa7e1facb1cc06abb44450d4ead005d1179ebd8f
nishant <nishant@debian12>
2025-01-10 13:56:19 +0530

    Database backup: 2025-01-10_13-56-19

--- backup_to_github.sh
+++ backup_to_github.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# GitHub repository HTTPS URL (replacing 'YOUR_TOKEN' with an environment variable for security)
+# Load PostgreSQL credentials from the configuration file
+source /home/nishant/Project/db-config/db_config.conf
+
+# GitHub repository HTTPS URL with token
 GIT_REPO="https://${GITHUB_TOKEN}@github.com/Nish1102/backup_cronjob.git"
 
 # Backup directory and file
@@ -10,9 +13,8 @@ BACKUP_FILE="$BACKUP_DIR/cricket_backup_$DATE.sql"
 
 # Step 1: Create a backup
 echo "Creating a backup of the database..."
-pg_dump -U postgres -d cricket -f $BACKUP_FILE
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U $POSTGRES_USER -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -f $BACKUP_FILE
 
-# Check if backup succeeded
 if [ $? -ne 0 ]; then
     echo "Database backup failed!" >&2
     exit 1
@@ -22,12 +24,10 @@ echo "Backup created successfully: $BACKUP_FILE"
 # Step 2: Push backup to GitHub
 echo "Pushing backup to GitHub repository..."
 cd $BACKUP_DIR
-git init
 git add .
 git commit -m "Database backup: $DATE"
 git push $GIT_REPO main
 
-# Check if push succeeded
 if [ $? -ne 0 ]; then
     echo "Failed to push backup to GitHub!" >&2
     exit 1
